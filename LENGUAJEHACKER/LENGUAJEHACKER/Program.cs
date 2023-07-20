ef52bf3a78877502317063020b4a73547554c29d
Grillo 404 <cdpabonjaimes@gmail.com>
2023-07-19 22:20:43 -0500

    Primera version y gitignore

---
+++ LENGUAJEHACKER/LENGUAJEHACKER/Program.cs
@@ -0,0 +1,40 @@
+﻿Dictionary<string, string> lenguaje = new Dictionary<string, string>() { 
+    { "A", "4"},
+    { "B", "I3"},
+    { "C", "["},
+    { "D", ")"},
+    { "MI", "3"},
+    { "F", "|="},
+    { "GRA", "&"},
+    { "H", "#"},
+    { "I", "1"},
+    { "J", ",_|"},
+    { "K", ">|"},
+    { "L", "1"},
+    { "MET", "/\\/\\"},
+    { "NORT", "^/"},
+    { "O", "0"},
+    { "PAG", "|*"},
+    { "Q", "(_,)"},
+    { "R", "I2"},
+    { "S", "5"},
+    { "T", "7"},
+    { "TU", "(_)"},
+    { "V", "\\/"},
+    { "W", "\\/\\/"},
+    { "X", "><"},
+    { "Y", "j"},
+    { "Z", "2"}};
+
+    string ins = Console.ReadLine().ToString();
+    string txtEncryptado = string.Empty;
+
+    foreach (var item in lenguaje)
+    {
+        ins = ins.ToLower().Replace(item.Key.ToLower(), item.Value);
+    }
+    
+    Console.WriteLine(ins);
+    Console.WriteLine(txtEncryptado);
+
+
---
+++ fizzBuzz/fizzBuzz/Program.cs
@@ -0,0 +1,13 @@
+﻿for (int i = 1; i <= 100; i++)
+{
+    string respuesta = string.Empty;
+    if (i % 3 == 0 || i % 5 == 0)
+    {
+        if (i % 3 == 0) respuesta = "Fizz";
+        if (i % 5 == 0) respuesta = respuesta + "Buzz";
+    }
+    else respuesta = i.ToString();
+    Console.WriteLine(respuesta);
+}
+
+Console.WriteLine("Progama finalizado");
