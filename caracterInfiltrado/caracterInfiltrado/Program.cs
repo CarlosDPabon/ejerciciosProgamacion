a291a50ac4f804c718d8bfaede946170c04d6aa5
Grillo 404 <cdpabonjaimes@gmail.com>
2023-07-19 23:30:36 -0500

    Reto caracter infiltrado

---
+++ caracterInfiltrado/caracterInfiltrado/Program.cs
@@ -0,0 +1,12 @@
+﻿string txtOriginal = "mi nombre es carlos";
+string txtAlterado = "mI nombro es farlos";
+List<string> caracteres = new List<string>();
+
+for (int i = 0; i < txtOriginal.Length; i++)
+{
+    if (txtOriginal.Substring(i, 1) != txtAlterado.Substring(i, 1)) caracteres.Add(txtAlterado.Substring(i, 1)); 
+}
+  
+Console.WriteLine(string.Concat(txtOriginal, " / ", txtAlterado, " -> ", string.Concat(caracteres)));
+
+
